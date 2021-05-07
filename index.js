9b3cbd6051165f7d3dd0053116bc3a7a1c601c34
Stephen Kurtz <stephenjkurtz@gmail.com>
2021-05-07 18:50:35 -0400

    add application code

--- index.js
+++ index.js
@@ -1,13 +1,66 @@
+function init() {
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js')
+const path = require('path');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'Title'
+        },
+        {
+            type: 'input',
+            message: 'Enter a description of your project.',
+            name: 'Description'
+        },
+        {
+            type: 'input',
+            message: 'Enter an installation guide.',
+            name: 'Installation'
+        },
+        {
+            type: 'input',
+            message: 'How do you use your application?',
+            name: 'Usage'
+        },
+        {
+            type: 'list',
+            message: 'Choose a license.',
+            choices: ['APACHE', 'GNU', 'IBM', 'MIT'],
+            name: 'License'
+        },
+        {
+            type: 'input',
+            message: 'How do others contribute?',
+            name: 'Contributing'
+        },
+        {
+            type: 'input',
+            message: 'How do others test your application?',
+            name: 'Tests'
+        },
+        {
+            type: 'input',
+            message: 'Provide GitHub account name',
+            name: 'Github'
+        },
+        {
+            type: 'input',
+            message: 'Provide Email',
+            name: 'Email'
+        }
+    ])
+    .then((response) => {
+        return fs.writeFileSync(path.join(process.cwd(), "README.md"), generateMarkdown(response));
+    });
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
+}
 
 // Function call to initialize app
 init();
+
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,59 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseBadge(data) {
+  const licenseType = data.License;
+  let licenseString = '';
+  if (licenseType === 'APACHE'){
+    licenseString = `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`;
+  }
+  if (licenseType === 'GNU'){
+    licenseString = `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`;
+  }
+  if (licenseType === 'IBM'){
+    licenseString = `[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)`;
+  }
+  if (licenseType === 'MIT'){
+    licenseString = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
+  }
+  return licenseString
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.Title}
 
-`;
+## Table of Content
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Usage](#usage)
+4. [License](#license)
+5. [Contributing](#contributing)
+6. [Tests](#test)
+7. [Questions](#qontact)
+
+## Description 
+${data.Description}
+
+## Installation 
+${data.Installation}
+
+## Usage
+${data.Usage}
+
+## License
+${renderLicenseBadge(data)}
+
+## Contributing 
+${data.Contributing}
+
+## Tests
+${data.Tests}
+
+## Questions 
+
+-Github: [GitHub](http://www.github.com/${data.Github})  
+-Email: ${data.Email}
+`
 }
 
 module.exports = generateMarkdown;
